a52587da3db7e64c3960ddb61affb3d47a8dc1cb
MAGSelPad <150753146+MAGSelPad@users.noreply.github.com>
2026-08-18 02:30:52 +0000

    Actualizar la ruta de la API para incluir 'index/' en lugar de la raíz

--- demo_rest_api/urls.py
+++ demo_rest_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("", views.DemoRestApi.as_view(), name="demo_rest_api_resources" ),
+   path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources" ),
    path("<str:id>/", views.DemoRestApiItem.as_view(), name="demo_rest_api_item" ),
 ]
